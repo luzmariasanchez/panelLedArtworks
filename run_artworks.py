37213d170717761f753c34fa818be986afe87969
Josué Martínez <luzmariasanchez.studio@gmail.com>
2024-04-26 22:54:56 -0600

    Create run_artworks.py

---
+++ run_artworks.py
@@ -0,0 +1,18 @@
+import datetime
+import os
+
+current_date = datetime.datetime.now()
+
+
+day_week = current_date.weekday()
+
+
+days = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+current_day = days[day_week]
+
+if current_day == days[1] or current_day == days[3] or current_day == days[5]:
+    print("Hoy es par")
+    os.system("python /home/pi/flag/flag.py")
+elif current_day == days[2] or current_day == days[4] or current_day == days[6]:
+    print("Hoy es inpar")
+    os.system("python /home/pi/detritus/detritus.py")
