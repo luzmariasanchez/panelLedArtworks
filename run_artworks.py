17c76ef313567a5c050f2a2144acdb1872734609
Josué Martínez <luzmariasanchez.studio@gmail.com>
2024-04-26 23:28:27 -0600

    Update run_artworks.py

--- run_artworks.py
+++ run_artworks.py
@@ -12,8 +12,8 @@ current_day = days[day_week]
 
 if current_day == days[1] or current_day == days[3] or current_day == days[5]:
     print("Hoy es par")
-    os.system("python /home/pi/detritus/detritus.py")
+    os.system("sudo python3 /home/pi/detritus/detritus.py")
 elif current_day == days[2] or current_day == days[4] or current_day == days[6]:
     print("Hoy es inpar")
-    os.system("python /home/pi/flag/flag.py")
+    os.system("python3 /home/pi/flag/flag.py")
     
